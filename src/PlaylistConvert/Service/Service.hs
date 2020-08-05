16d48e9a589d79cca188b7d150342cb699529498
Raven's Iris <ravens.iris.mx@gmail.com>
2020-08-05 04:44:49 +0200

    Families test

--- src/PlaylistConvert/Service/Service.hs
+++ src/PlaylistConvert/Service/Service.hs
@@ -1,19 +1,39 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module PlaylistConvert.Service where
 
 import qualified Data.Text as T
 
-type Page = Int
+data family Service a
 
-newtype Match a = Match a
+instance Show (Service a)
 
+data Deezer = DeezerService deriving Show
+data Tidal = TidalService deriving Show
 
-data Track = Track { trackTitle :: T.Text
+data instance Service Deezer = Deezer 
+data instance Service Tidal = Tidal
+
+instance Action (Service Deezer) where
+  search a b = [dummyTrack (Deezer)]
+instance Action (Service Tidal) where
+  search a b = [dummyTrack (Tidal)]
+
+class Action a where
+  search :: a -> T.Text -> [Track a]
+
+data Id a = Id a T.Text deriving Show
+
+data Track a = Track { trackTitle :: T.Text
                    , trackAlbum :: T.Text
                    , trackArtists :: [T.Text]
-                   , trackId :: Id
+                   , trackId :: a
                    } deriving Show
 
-class Service a where
-  paginatedSearchTracks :: a -> Page -> Maybe (Match [Track])
+dummyTrack s = Track s "Title" "Album" ["Artist A", "Artist B"]
 
+main :: IO ()
+main = print $ search Tidal ""
 
